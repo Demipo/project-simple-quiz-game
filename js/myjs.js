c8bea87a09ace801c129c43d4b385ced9d9b1984
Onaademipo <onaade85@yahoo.com>
2019-09-17 20:11:23 +0100

    Updated db.json, js/myjs.js and upload.html files

--- js/myjs.js
+++ js/myjs.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-    $('.submitButton').click(function(event) {
-        event.preventDefault();
-        const question = $('#question').val();
-        const optionOne = $('#optionOne').val();
-        const optionTwo = $('#optionTwo').val();
-        const optionThree = $('#optionThree').val();
-        const optionFour = $('#optionFour').val();
-        const answer = $('#answer').val();
-        //Check if user input is empty
-        if (!question || !optionOne || !optionTwo || !optionThree || !optionFour || !answer) {
-          $('.uploadMessage').html('Kindly fill in all fields');
-          return;
-        }
-    $.ajax({
-        method: 'POST',
-        url: 'http://localhost:3000/questions',
-        data: {
-          question,
-          optionOne,
-          optionTwo,
-          optionThree,
-          optionFour,
-          answer,
-        },
-        beforeSend: function() {
-          $('.uploadMessage').html('Loading....');
-        },
-        success: function() {
-          $('.uploadMessage').html('Question loaded successfully');
-        },
+  $('.submitButton').click(function(event) {
+      event.preventDefault();
+      const question = $('#question').val();
+      const optionOne = $('#optionOne').val();
+      const optionTwo = $('#optionTwo').val();
+      const optionThree = $('#optionThree').val();
+      const optionFour = $('#optionFour').val();
+      const answer = $('#answer').val();
+      //Check if user input is empty
+      if (!question || !optionOne || !optionTwo || !optionThree || !optionFour || !answer) {
+        $('.uploadMessage').html('Kindly fill in all fields');
+        return;
+      }
+  $.ajax({
+      method: 'POST',
+      url: 'http://localhost:3000/questions',
+      data: {
+        question,
+        optionOne,
+        optionTwo,
+        optionThree,
+        optionFour,
+        answer,
+      },
+      beforeSend: function() {
+        $('.uploadMessage').html('Loading....');
+      },
+      success: function() {
+        $('.uploadMessage').html('Question loaded successfully');
+      },
 });
-    })
+  })
 })

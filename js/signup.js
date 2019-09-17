bd2ecb0242a34e537c172e508ec7deeb5061afd3
Onaademipo <onaade85@yahoo.com>
2019-09-17 22:59:52 +0100

    Modified js/signup.js

--- js/signup.js
+++ js/signup.js
@@ -49,45 +49,6 @@ $(document).ready(function() {
         },
       });
     });
-    //Login Function
-    $('.loginSubmitBtn').click(function(event) {
-      event.preventDefault();
-      const passwordLogin = $('#passwordLogin').val();
-      const emailLogin = $('#emailLogin').val();
-      if (!passwordLogin || !emailLogin) {
-        $('.regMessage').html('Kindly fill in all fields');
-        return;
-      }
-      //Check if the user is in the database
-      $.ajax({
-        method: 'GET',
-        url: `http://localhost:3000/users?email=${emailLogin}&password=${passwordLogin}`,
-        data: {
-          email: emailLogin,
-          password: passwordLogin,
-        },
-        beforeSend: function() {
-          $('.regMessage').html('Loading....');
-        },
-        success: function(response) {
-          if (response.length) {
-            $('.regMessage').html('Login sucessful');
-            $('.checkLogin').html('You are logged in');
-            localStorage.setItem('email', emailLogin);
-            //redirect to home page if the login is successfull
-            window.location.assign('index.html');
-          } else {
-            $('.regMessage').html('Username or password Incorrect');
-          }
-        },
-      });
-    });
-    //Logout Function
-    $('.logoutBtn').click(function() {
-      //clear the localstorage and redirect to signup page
-      localStorage.clear();
-      $('.checkLogin').html('Kindly login');
-      window.location.assign('signup.html');
-    });
+    
   });
   

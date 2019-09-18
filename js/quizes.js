8cb64ee6af7d75ec5797539d0a22f996f86ff723
Onaademipo <onaade85@yahoo.com>
2019-09-18 16:34:06 +0100

    Updated js/quizes

--- js/quizes.js
+++ js/quizes.js
@@ -3,15 +3,22 @@ $(document).ready(function() {
                
 
     $(".playButton").click(function(event){ 
+        $.getJSON('http:localhost:3000/questions', function(quest) {  
+           $.each(quest, function(key, value) {
+                const display =
+                `<h3>Q</h3><h3>${value.id}</h3><br>
+                    <h4>${value.question}</h4><br>
+                    <input class="dot" type="radio" name="option" value="B">${value.optionOne}
+                    <input class="dot" type="radio" name="option" value="C">${value.optionTwo}
+                    <input class="dot" type="radio" name="option" value="D">${value.optionThree}
+                    <input class="dot" type="radio" name="option" value="D">${value.optionFour}
+                    <hr>`;
+            $(".form1").append(display)
+       })
+           
+        })
 
-        $.getJSON('http://localhost:3000/questions', function(quest) { 
-
-            $('.form1').html('<p> Name: ' + quest.[1]["question"] + '</p>'); 
-
-
-
-        }); 
-
+        $("#header").text("Have fun!!");
+    })  
+        
     }); 
-
-}); 
